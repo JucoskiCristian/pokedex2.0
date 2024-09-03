d9acfa6aae78ca89ae773a57a51fa991c74796e5
Cristian Jucoski <cristian.jucoski@gmail.com>
2024-09-03 19:29:31 -0300

    Correção nos li, logo temporaria

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { card } from './components/card'
 
 type Pokemon = {
   id: number
   name: string
+  order: number
   url: string
   types: { type: { name: string } }[]
   sprites: {
@@ -19,7 +19,6 @@ type Pokemon = {
 export function App() {
   const [pokemonData, setPokemonData] = useState<Pokemon[]>([])
   const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState<unknown | null>(null)
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -39,7 +38,7 @@ export function App() {
         )
         setPokemonData(pokemonDetails)
       } catch (error) {
-        setError(error)
+        console.log(error)
       } finally {
         setIsLoading(false)
       }
@@ -51,26 +50,25 @@ export function App() {
     return <div>Loading...</div>
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>
-  }
-
   return (
-    <main className="mx-auto h-dvh max-w-screen-xl bg-zinc-200">
-      <h1 className="p-6 text-center text-5xl text-yellow-400">Pokedex</h1>
-      <section>
-        <ul className="flex flex-wrap justify-center gap-4">
+    <main className="mx-auto h-full max-w-screen-xl bg-slate-200">
+      {/* logo temporario */}
+      <img className="mx-auto" src="src/img/pokedexlogo.png" alt="" />
+      <section className="py-7">
+        <ul className="flex flex-wrap justify-center gap-x-6 gap-y-16">
           {pokemonData.map((pokemon) => (
             <li
               key={pokemon.id}
-              className={`bg-${pokemon.types[0].type.name} h-52 w-52 rounded-3xl bg-slate-200`}>
+              className={`bg-${pokemon.types[0].type.name} relative h-36 w-52 rounded-3xl bg-white shadow-lg`}>
               <div className="flex justify-center">
                 <img
+                  className="absolute -top-12 h-24 w-24 object-scale-down"
                   src={pokemon.sprites.other.showdown.front_default}
                   alt={pokemon.name}
                 />
               </div>
-              <div className="flex flex-col items-center justify-center p-3">
+              <div className="absolute inset-x-0 bottom-0 flex flex-col items-center space-y-1 p-3">
+                <h2 className="text-xs text-neutral-500">N° {pokemon.order}</h2>
                 <h1 className="capitalize">{pokemon.name}</h1>
                 <div className="flex gap-2">
                   {pokemon.types.map((type) => (
